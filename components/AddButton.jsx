2ce9240cbe5b101337d2ea64c787a754bb3ab703
Luis Casas <luisscasas45@gmail.com>
2024-09-17 19:01:24 -0600

    FEAT/ create AddButton component with basic styles

---
+++ components/AddButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddButton = () => {
+  return (
+    <button className="bg-gradient-to-r from-[#21262D] to-[#414B66] text-white px-4 py-2 rounded-lg hover:bg-opacity-80 transition-all">
+      Crear equipo
+    </button>
+  );
+};
+
+export default AddButton;
+
+
--- pages/catalogo.jsx
+++ pages/catalogo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import Sidebar from '../components/Sidebar'; 
+import Sidebar from '../components/Sidebar';
+import AddButton from '../components/AddButton'; // Importar el componente AddButton
 
 const Catalogo = () => {
   return (
@@ -8,11 +9,15 @@ const Catalogo = () => {
       <Sidebar /> 
       <main className="flex-1 p-6">
         <SearchBar />
+        <div className="mt-6 flex justify-end">
+          <AddButton /> 
+        </div>
       </main>
     </div>
   );
 };
 
 export default Catalogo;
+
 
 
