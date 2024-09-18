b315f050cf04f6733c63dc2b5db79e2aa9737e57
Joseph Colin <joelbenjosephc@gmail.com>
2024-09-18 13:21:42 -0600

    Se agregaron los cambios de la creacion de usuarios

--- pages/ActualizarUsuario.jsx
+++ pages/ActualizarUsuario.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */  
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import Link from 'next/link'
+
 
 export default function CreacionDeUsuarios() {
   const {
@@ -17,185 +19,223 @@ export default function CreacionDeUsuarios() {
   }
 
   return (
-    <main className='bg-lightblu'>
-      <div className='relative'>
-        <nav className='flex items-center justify-between p-4 bg-gray-100'>
-          <div className='flex items-center space-x-4 w-full'>
-            <button className='p-2 focus:outline-none' onClick={toggleMenu}>
-              <svg
-                className='w-6 h-6'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
+    <div className='flex flex-col min-h-screen'>
+      <main className='flex-grow'>
+        <div className='relative'>
+          <nav className='flex items-center justify-between p-4 bg-gray-100'>
+            <div className='flex items-center space-x-4 w-full'>
+              <button className='p-2 focus:outline-none' onClick={toggleMenu}>
+                <svg
+                  className='w-6 h-6'
+                  fill='none'
+                  stroke='currentColor'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h16m-7 6h7'
+                  ></path>
+                </svg>
+              </button>
+              <input
+                type='text'
+                placeholder='Buscar...'
+                className='w-full p-2 border border-gray-300 rounded'
+              />
+            </div>
+            <div className='ml-2 mt-1'>
+              <Link href='/catalogoDeUsuarios'>
+                <button className='text-blue-500 hover:text-blue-700'>
+                  <svg
+                    className='w-6 h-6'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M15 19l-7-7 7-7'
+                    ></path>
+                  </svg>
+                </button>
+              </Link>
+            </div>
+          </nav>
+          {isOpen && (
+            <div className='absolute top-0 left-0 w-full bg-white shadow-md'>
+              <button
+                className='absolute top-2 right-2 p-2 focus:outline-none'
+                onClick={toggleMenu}
               >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16m-7 6h7'
-                ></path>
-              </svg>
-            </button>
-            <input
-              type='text'
-              placeholder='Buscar...'
-              className='w-full p-2 border border-gray-300 rounded'
-            />
-          </div>
-        </nav>
-        {isOpen && (
-          <div className='absolute top-0 left-0 w-full bg-white shadow-md'>
-            <button
-              className='absolute top-2 right-2 p-2 focus:outline-none'
-              onClick={toggleMenu}
-            >
-              <svg
-                className='w-6 h-6'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M6 18L18 6M6 6l12 12'
-                ></path>
-              </svg>
-            </button>
-            <ul>
-              <li className='p-4 border-b border-gray-200'>
-                <a href='#home'>Home</a>
-              </li>
-              <li className='p-4 border-b border-gray-200'>
-                <a href='#about'>About</a>
-              </li>
-              <li className='p-4 border-b border-gray-200'>
-                <a href='#contact'>Contact</a>
-              </li>
-            </ul>
-          </div>
-        )}
-      </div>
-      <div>
-        <div className='text-2xl font-semibold text-center my-4'>
-          Crear Perfil
+                <svg
+                  className='w-6 h-6'
+                  fill='none'
+                  stroke='currentColor'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M6 18L18 6M6 6l12 12'
+                  ></path>
+                </svg>
+              </button>
+              <ul>
+                <li className='p-4 border-b border-gray-200'>
+                  <a href='#home'>Home</a>
+                </li>
+                <li className='p-4 border-b border-gray-200'>
+                  <a href='#about'>About</a>
+                </li>
+                <li className='p-4 border-b border-gray-200'>
+                  <a href='#contact'>Contact</a>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className='max-w-md mx-auto p-4 border border-gray-300 rounded'
-        >
-          <div className='mb-4'>
-            <label
-              htmlFor='nombre'
-              className='block text-sm font-medium text-gray-700'
-            >
-              Nombre Completo
-            </label>
-            <input
-              type='text'
-              id='nombre'
-              placeholder='Juanito Perez Gonzalez'
-              {...register('nombre', { required: true })}
-              className='mt-1 p-2 block w-full border border-gray-300 rounded'
-            />
-            {errors.nombre && (
-              <span className='text-red-500 text-sm'>
-                Este campo es requerido
-              </span>
-            )}
+        <div>
+          <div className='text-2xl font-semibold text-center my-4'>
+            Actualizar Perfil
           </div>
-          <div className='mb-4'>
-            <label
-              htmlFor='email'
-              className='block text-sm font-medium text-gray-700'
-            >
-              Email
-            </label>
-            <input
-              type='email'
-              id='email'
-              placeholder='Correo@dominio.com'
-              {...register('email', { required: true })}
-              className='mt-1 p-2 block w-full border border-gray-300 rounded'
-            />
-            {errors.email && (
-              <span className='text-red-500 text-sm'>
-                Este campo es requerido
-              </span>
-            )}
-          </div>
-          <div className='mb-4'>
-            <label
-              htmlFor='password'
-              className='block text-sm font-medium text-gray-700'
-            >
-              Contraseña
-            </label>
-            <input
-              type='password'
-              id='password'
-              placeholder='********'
-              {...register('password', { required: true })}
-              className='mt-1 p-2 block w-full border border-gray-300 rounded'
-            />
-            {errors.password && (
-              <span className='text-red-500 text-sm'>
-                Este campo es requerido
-              </span>
-            )}
-          </div>
-          <div className='mb-4'>
-            <label
-              htmlFor='cargo'
-              className='block text-sm font-medium text-gray-700'
-            >
-              Cargo
-            </label>
-            <input
-              type='text'
-              id='cargo'
-              placeholder='Ingeniero'
-              {...register('cargo', { required: true })}
-              className='mt-1 p-2 block w-full border border-gray-300 rounded'
-            />
-            {errors.cargo && (
-              <span className='text-red-500 text-sm'>
-                Este campo es requerido
-              </span>
-            )}
-          </div>
-          <div className='mb-4'>
-            <label
-              htmlFor='foto'
-              className='block text-sm font-medium text-gray-700'
-            >
-              Foto
-            </label>
-            <input
-              type='file'
-              id='foto'
-              {...register('foto', { required: true })}
-              className='mt-1 p-2 block w-full border border-gray-300 rounded'
-            />
-            {errors.foto && (
-              <span className='text-red-500 text-sm'>
-                Este campo es requerido
-              </span>
-            )}
-          </div>
-          <div className='text-center'>
-            <button
-              type='submit'
-              className='p-2 bg-blue-500 text-white rounded'
-            >
-              Actualizar
-            </button>
-          </div>
-        </form>
-      </div>
-    </main>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='max-w-md mx-auto p-4 border border-gray-300 rounded'
+          >
+            <div className='mb-4'>
+              <label
+                htmlFor='Id'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Id
+              </label>
+              <input
+                type='text'
+                id='id'
+                placeholder='6663cd141e150815e43d0a57'
+                {...register('id', { required: true })}
+                className='mt-1 p-2 block w-full border border-gray-300 rounded'
+                readOnly
+              />
+            </div>
+            <div className='mb-4'>
+              <label
+                htmlFor='nombre'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Nombre Completo
+              </label>
+              <input
+                type='text'
+                id='nombre'
+                placeholder='Juanito Perez Gonzalez'
+                {...register('nombre', { required: true })}
+                className='mt-1 p-2 block w-full border border-gray-300 rounded'
+              />
+              {errors.nombre && (
+                <span className='text-red-500 text-sm'>
+                  Este campo es requerido
+                </span>
+              )}
+            </div>
+            <div className='mb-4'>
+              <label
+                htmlFor='email'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Email
+              </label>
+              <input
+                type='email'
+                id='email'
+                placeholder='Correo@dominio.com'
+                {...register('email', { required: true })}
+                className='mt-1 p-2 block w-full border border-gray-300 rounded'
+              />
+              {errors.email && (
+                <span className='text-red-500 text-sm'>
+                  Este campo es requerido
+                </span>
+              )}
+            </div>
+            <div className='mb-4'>
+              <label
+                htmlFor='password'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Contraseña
+              </label>
+              <input
+                type='password'
+                id='password'
+                placeholder='********'
+                {...register('password', { required: true })}
+                className='mt-1 p-2 block w-full border border-gray-300 rounded'
+              />
+              {errors.password && (
+                <span className='text-red-500 text-sm'>
+                  Este campo es requerido
+                </span>
+              )}
+            </div>
+            <div className='mb-4'>
+              <label
+                htmlFor='cargo'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Cargo
+              </label>
+              <input
+                type='text'
+                id='cargo'
+                placeholder='Ingeniero'
+                {...register('cargo', { required: true })}
+                className='mt-1 p-2 block w-full border border-gray-300 rounded'
+              />
+              {errors.cargo && (
+                <span className='text-red-500 text-sm'>
+                  Este campo es requerido
+                </span>
+              )}
+            </div>
+            <div className='mb-4'>
+              <label
+                htmlFor='foto'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Foto
+              </label>
+              <input
+                type='file'
+                id='foto'
+                {...register('foto', { required: false })}
+                className='mt-1 p-2 block w-full border border-gray-300 rounded'
+              />
+            </div>
+            <div className='text-center'>
+              <button
+                type='submit'
+                className='p-2 bg-blue-500 text-white rounded'
+              >
+                Actualizar
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+      <footer className='mt-8 p-4 bg-gray-200 text-center'>
+        <p className='text-sm text-gray-600'>
+          © 2024 ManteniPro. Todos los derechos reservados.
+        </p>
+      </footer>
+    </div>
   )
 }
--- pages/catalogoDeUsuarios.jsx
+++ pages/catalogoDeUsuarios.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from 'react'
+import Link from 'next/link'
 
 export default function CatalogoDeUsuarios() {
   const [isOpen, setIsOpen] = useState(false)
@@ -8,113 +9,21 @@ export default function CatalogoDeUsuarios() {
     setIsOpen(!isOpen)
   }
 
+ const [isModalOpen, setIsModalOpen] = useState(false)
+
+ const handleDelete = () => {
+   // Lógica para borrar el usuario
+   console.log('Usuario borrado')
+   setIsModalOpen(false)
+ }
+
   return (
-    <main className='bg-lightblue'>
-      <div className='relative'>
-        <nav className='flex items-center justify-between p-4 bg-gray-100'>
-          <div className='flex items-center space-x-4 w-full'>
-            <button className='p-2 focus:outline-none' onClick={toggleMenu}>
-              <svg
-                className='w-6 h-6'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16m-7 6h7'
-                ></path>
-              </svg>
-            </button>
-            <input
-              type='text'
-              placeholder='Buscar...'
-              className='w-full p-2 border border-gray-300 rounded'
-            />
-            <button className=' text-blue-500 hover:text-blue-700'>
-              <svg
-                className='w-6 h-6'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M15 12H9m4 0V8m0 4v4m-4-4h4m0 0H9m4 0h4'
-                ></path>
-              </svg>
-            </button>
-          </div>
-        </nav>
-        {isOpen && (
-          <div className='absolute top-0 left-0 w-full bg-white shadow-md'>
-            <button
-              className='absolute top-2 right-2 p-2 focus:outline-none'
-              onClick={toggleMenu}
-            >
-              <svg
-                className='w-6 h-6'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M6 18L18 6M6 6l12 12'
-                ></path>
-              </svg>
-            </button>
-            <ul>
-              <li className='p-4 border-b border-gray-200'>
-                <a href='#home'>Home</a>
-              </li>
-              <li className='p-4 border-b border-gray-200'>
-                <a href='#home'>Creacion de Usuarios</a>
-              </li>
-              <li className='p-4 border-b border-gray-200'>
-                <a href='#contact'>About</a>
-              </li>
-            </ul>
-          </div>
-        )}
-      </div>
-      <div>
-        <div className='text-2xl font-semibold text-center my-4'>
-          Catálogo de Usuarios
-        </div>
-        <div className='flex justify-center my-4'>
-          <div className='flex items-center space-x-4'>
-            <label htmlFor='sort' className='text-sm'>
-              Ordenar por:
-            </label>
-            <select id='sort' className='p-2 border border-gray-300 rounded'>
-              <option value='date'>Fecha de creación</option>
-              <option value='name'>Nombre</option>
-            </select>
-          </div>
-        </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:gap-2 items-center'>
-          <div className='flex flex-row sm:flex-row sm:mb-0 items-center p-4 border mx-0.5 border-gray-300 rounded mb-4 w-full max-w-full'>
-            <img
-              src='https://via.placeholder.com/50'
-              alt='User'
-              className='w-12 h-12 rounded-full'
-            />
-            <div className='ml-4'>
-              <div className='text-lg font-semibold'>Nombre del Usuario</div>
-              <div className='text-sm text-gray-600'>correo@ejemplo.com</div>
-            </div>
-            <div className='ml-auto flex space-x-4 mt-4 sm:mt-0'>
-              <button className='p-2 text-blue-500 hover:text-blue-700'>
+    <div className='flex flex-col min-h-screen'>
+      <main className='flex-grow'>
+        <div className='relative'>
+          <nav className='flex items-center justify-between p-4 bg-gray-100'>
+            <div className='flex items-center space-x-4 w-full'>
+              <button className='p-2 focus:outline-none' onClick={toggleMenu}>
                 <svg
                   className='w-6 h-6'
                   fill='none'
@@ -126,11 +35,41 @@ export default function CatalogoDeUsuarios() {
                     strokeLinecap='round'
                     strokeLinejoin='round'
                     strokeWidth='2'
-                    d='M15 12H9m4 0V8m0 4v4m-4-4h4m0 0H9m4 0h4'
+                    d='M4 6h16M4 12h16m-7 6h7'
                   ></path>
                 </svg>
               </button>
-              <button className='p-2 text-red-500 hover:text-red-700'>
+              <input
+                type='text'
+                placeholder='Buscar...'
+                className='w-full p-2 border border-gray-300 rounded'
+              />
+              <Link href='/CreacionDeUsuarios'>
+                <button className=' text-blue-500 hover:text-blue-700'>
+                  <svg
+                    className='w-6 h-6'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M15 12H9m4 0V8m0 4v4m-4-4h4m0 0H9m4 0h4'
+                    ></path>
+                  </svg>
+                </button>
+              </Link>
+            </div>
+          </nav>
+          {isOpen && (
+            <div className='absolute top-0 left-0 w-full bg-white shadow-md'>
+              <button
+                className='absolute top-2 right-2 p-2 focus:outline-none'
+                onClick={toggleMenu}
+              >
                 <svg
                   className='w-6 h-6'
                   fill='none'
@@ -146,10 +85,115 @@ export default function CatalogoDeUsuarios() {
                   ></path>
                 </svg>
               </button>
+              <ul>
+                <li className='p-4 border-b border-gray-200'>
+                  <a href='#home'>Home</a>
+                </li>
+                <li className='p-4 border-b border-gray-200'>
+                  <a href='#home'>Plan</a>
+                </li>
+                <li className='p-4 border-b border-gray-200'>
+                  <a href='#contact'>About</a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div>
+          <div className='text-2xl font-semibold text-center my-4'>
+            Catálogo de Usuarios
+          </div>
+          <div className='flex justify-center my-4'>
+            <div className='flex items-center space-x-4'>
+              <label htmlFor='sort' className='text-sm'>
+                Ordenar por:
+              </label>
+              <select id='sort' className='p-2 border border-gray-300 rounded'>
+                <option value='date'>Fecha de creación</option>
+                <option value='name'>Nombre</option>
+              </select>
+            </div>
+          </div>
+          <div className='grid grid-cols-1 sm:grid-cols-2 sm:gap-2 items-center'>
+            <div className='flex flex-row sm:flex-row sm:mb-0 items-center p-4 border mx-0.5 border-gray-300 rounded mb-4 w-full max-w-full'>
+              <img
+                src='https://via.placeholder.com/50'
+                alt='User'
+                className='w-12 h-12 rounded-full'
+              />
+              <div className='ml-4'>
+                <div className='text-lg font-semibold'>Nombre del Usuario</div>
+                <div className='text-sm text-gray-600'>correo@ejemplo.com</div>
+              </div>
+              <div className='ml-auto flex space-x-4 mt-4 sm:mt-0'>
+                <Link href='ActualizarUsuario'>
+                  <button className='p-2 text-blue-500 hover:text-blue-700'>
+                    <svg
+                      className='w-6 h-6'
+                      fill='none'
+                      stroke='currentColor'
+                      viewBox='0 0 24 24'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M15 12H9m4 0V8m0 4v4m-4-4h4m0 0H9m4 0h4'
+                      ></path>
+                    </svg>
+                  </button>
+                </Link>
+                <button
+                  onClick={() => setIsModalOpen(true)}
+                  className='p-2 text-red-500 hover:text-red-700'
+                >
+                  <svg
+                    className='w-6 h-6'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M6 18L18 6M6 6l12 12'
+                    ></path>
+                  </svg>
+                </button>
+                {isModalOpen && (
+                  <div className='fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50'>
+                    <div className='bg-white p-4 rounded'>
+                      <p>¿Estás seguro de que deseas borrar este usuario?</p>
+                      <div className='flex justify-end mt-4'>
+                        <button
+                          onClick={() => setIsModalOpen(false)}
+                          className='mr-2 p-2 bg-gray-300 rounded'
+                        >
+                          Cancelar
+                        </button>
+                        <button
+                          onClick={handleDelete}
+                          className='p-2 bg-red-500 text-white rounded'
+                        >
+                          Borrar
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+      <footer className='mt-8 p-4 bg-gray-200 text-center'>
+        <p className='text-sm text-gray-600'>
+          © 2024 ManteniPro. Todos los derechos reservados.
+        </p>
+      </footer>
+    </div>
   )
 }
