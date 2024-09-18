849b0a446d5c1a8d203d49aa464592108e8683e9
Luis Casas <luisscasas45@gmail.com>
2024-09-17 19:01:57 -0600

    FEAT/ add Source Sans 3 font to button

--- components/AddButton.jsx
+++ components/AddButton.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Source_Sans_3 } from 'next/font/google';
+
+const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
 const AddButton = () => {
   return (
-    <button className="bg-gradient-to-r from-[#21262D] to-[#414B66] text-white px-4 py-2 rounded-lg hover:bg-opacity-80 transition-all">
+    <button className={`bg-gradient-to-r from-[#21262D] to-[#414B66] text-white px-4 py-2 rounded-lg hover:bg-opacity-80 transition-all ${sourceSans3.className}`}>
       Crear equipo
     </button>
   );
 };
 
 export default AddButton;
+
 
 
