7533feaebc50111626350f20e676b4d662d2f5a8
Luis Casas <luisscasas45@gmail.com>
2024-09-17 18:37:57 -0600

    FEAT/ Styling for SearchBar component

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Source_Sans_3 } from 'next/font/google';
 import Image from 'next/image';
-import searchIcon from '../public/icon/search-icon.png';
+import searchIcon from '../public/icon/search-icon.png'; 
 
 const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
 const SearchBar = () => {
   return (
-    <div className="w-full max-w-lg mx-auto flex items-center p-2 rounded-lg">
+    <div className={`w-full max-w-lg mx-auto flex items-center p-2 rounded-lg ${sourceSans3.className}`} 
+         style={{ background: 'linear-gradient(90deg, #21262D 12%, #414B66 88%)' }}> 
       <Image 
         src={searchIcon} 
         alt="Search Icon" 
@@ -16,13 +17,14 @@ const SearchBar = () => {
       <input 
         type="text" 
         placeholder="Buscar usuario" 
-        className="w-full"
+        className="w-full bg-transparent focus:outline-none text-white p-2"
       />
     </div>
   );
 };
 
 export default SearchBar;
+
 
 
 
