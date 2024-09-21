f48139374bbe2654b29b681ac6479d7dbc5995b5
Luis Casas <luisscasas45@gmail.com>
2024-09-20 23:25:05 -0600

    FEAT/ add TicketsDashboard page

---
+++ pages/tickets_dashboard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+import TempSidebar from '../components/TempSidebar';
+import Title from '../components/Title';
+import BurgerMenu from '../components/BurgerMenu';
+
+const TicketsDashboard = () => {
+  return (
+    <div className="min-h-screen bg-white flex relative">
+      <div className="relative">
+        <TempSidebar /> 
+      </div>
+
+      
+      <main className="flex-1 p-6">
+        <div className="flex items-center justify-between mb-6">
+          <BurgerMenu className="text-sm" /> 
+          <SearchBar className="w-1/2 md:w-1/3" />
+        </div>
+
+        
+        <div className="mb-4">
+          <Title className="text-2xl">Tickets</Title> 
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default TicketsDashboard;
+
+
