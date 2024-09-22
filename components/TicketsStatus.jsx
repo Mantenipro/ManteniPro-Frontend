23617a5804e3927704b0726684ec39bfa1f77804
Luis Casas <luisscasas45@gmail.com>
2024-09-22 16:17:41 -0600

    FEAT/ make scrollbar global for tickets sections

--- components/TicketsStatus.jsx
+++ components/TicketsStatus.jsx
@@ -78,7 +78,8 @@ const StatusColumn = ({ title, tickets, handleNextSection, handlePrevSection, sh
 
     <div className="w-full h-1 transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 bg-gradient-to-r from-[#21262D] to-[#414B66]"></div>
 
-    <div className="w-full mt-4 group-hover:bg-opacity-100">
+    {/* Contenedor scrollable para los tickets */}
+    <div className="w-full mt-4 group-hover:bg-opacity-100 h-[470px] md:h-[385px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
       {tickets.length === 0 ? (
         <p>No hay tickets para mostrar</p>
       ) : (
@@ -99,6 +100,10 @@ const StatusColumn = ({ title, tickets, handleNextSection, handlePrevSection, sh
 );
 
 export default TicketsStatus;
+
+
+
+
 
 
 
