3a92da142fd16346fd66f0a4e20424d79646c251
Luis Casas <luisscasas45@gmail.com>
2024-09-22 19:02:32 -0600

    FEAT/ Remove filtering from PriorityFilter

--- components/PriorityFilter.jsx
+++ components/PriorityFilter.jsx
@@ -7,19 +7,11 @@ const priorityOptions = [
   { label: 'Sin Prioridad', value: 'Sin Prioridad', color: 'bg-gray-500' },
 ];
 
-const PriorityFilter = ({ selectedPriorities, setSelectedPriorities }) => {
+const PriorityFilter = () => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [dropdownPosition, setDropdownPosition] = useState({ top: 0, left: 0 });
   const buttonRef = useRef(null);
   const dropdownRef = useRef(null);
-
-  const handleCheckboxChange = (option) => {
-    if (selectedPriorities.includes(option)) {
-      setSelectedPriorities(selectedPriorities.filter((priority) => priority !== option));
-    } else {
-      setSelectedPriorities([...selectedPriorities, option]);
-    }
-  };
 
   useEffect(() => {
     if (buttonRef.current && showDropdown) {
@@ -72,20 +64,11 @@ const PriorityFilter = ({ selectedPriorities, setSelectedPriorities }) => {
               <li
                 key={value}
                 className="flex items-center justify-between p-1 hover:bg-blue-100 cursor-pointer text-xs md:text-sm"
-                onClick={() => handleCheckboxChange(value)}
               >
                 <div className="flex items-center">
                   <span className={`w-3 h-3 rounded-full ${color} mr-2 md:w-4 md:h-4`} />
-                  <span className={selectedPriorities.includes(value) ? 'font-medium' : ''}>
-                    {label}
-                  </span>
+                  <span>{label}</span>
                 </div>
-                <input
-                  type="checkbox"
-                  checked={selectedPriorities.includes(value)}
-                  readOnly
-                  className="h-3 w-3 md:h-4 md:w-4"
-                />
               </li>
             ))}
           </ul>
@@ -96,6 +79,7 @@ const PriorityFilter = ({ selectedPriorities, setSelectedPriorities }) => {
 };
 
 export default PriorityFilter;
+
 
 
 
