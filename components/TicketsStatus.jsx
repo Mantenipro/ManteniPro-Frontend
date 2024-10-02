8542a44c16e31e00d4712dad431d5b66253837a6
Luis Casas <luisscasas45@gmail.com>
2024-10-02 16:36:07 -0600

    FEAT/ padding cards 2px

--- components/TicketsStatus.jsx
+++ components/TicketsStatus.jsx
@@ -35,6 +35,7 @@ const TicketsStatus = ({ ticketsPorHacer, ticketsEnProceso, ticketsCompletados, 
   return (
     <div className="bg-[#F5F5F5] rounded-lg">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 text-xs">
+        
         <StatusColumn 
           title={sections[currentSection].title} 
           tickets={filterTicketsByPriority(sections[currentSection].tickets)} 
@@ -42,6 +43,7 @@ const TicketsStatus = ({ ticketsPorHacer, ticketsEnProceso, ticketsCompletados, 
           handlePrevSection={handlePrevSection}
           showNavigation={true} 
         />
+      
         <div className="hidden md:block">
           <StatusColumn title="En proceso" tickets={filterTicketsByPriority(ticketsEnProceso)} />
         </div>
@@ -54,12 +56,13 @@ const TicketsStatus = ({ ticketsPorHacer, ticketsEnProceso, ticketsCompletados, 
 };
 
 const StatusColumn = ({ title, tickets, handleNextSection, handlePrevSection, showNavigation }) => (
-  <div className="flex flex-col items-center group">
-    <div className="flex items-center justify-center w-full mb-4 mt-2">
+  <div className="relative flex flex-col items-center group">
+    
+    <div className="relative flex items-center justify-center w-full mb-4 mt-2">
       {showNavigation && (
         <button 
           onClick={handlePrevSection} 
-          className="block md:hidden bg-gray-200 p-2 rounded-full"
+          className="absolute left-2 top-1/2 transform -translate-y-1/2 block md:hidden bg-gray-200 p-2 rounded-full"
         >
           <img src="/icon/left-arrow-icon.png" alt="Left arrow" className="w-4 h-4" />
         </button>
@@ -71,7 +74,7 @@ const StatusColumn = ({ title, tickets, handleNextSection, handlePrevSection, sh
       {showNavigation && (
         <button 
           onClick={handleNextSection} 
-          className="block md:hidden bg-gray-200 p-2 rounded-full"
+          className="absolute right-2 top-1/2 transform -translate-y-1/2 block md:hidden bg-gray-200 p-2 rounded-full"
         >
           <img src="/icon/right-arrow-icon.png" alt="Right arrow" className="w-4 h-4" />
         </button>
@@ -80,13 +83,13 @@ const StatusColumn = ({ title, tickets, handleNextSection, handlePrevSection, sh
 
     <div className="w-full h-1 transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 bg-gradient-to-r from-[#21262D] to-[#414B66]"></div>
 
-    {/* Contenedor scrollable para los tickets */}
+    
     <div className="flex flex-wrap justify-center w-full mt-4 group-hover:bg-opacity-100 h-[550px] md:h-[445px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
       {tickets.length === 0 ? (
         <p>No hay tickets para mostrar</p>
       ) : (
         tickets.map((ticket) => (
-          <div key={ticket.ticketId} className="m-0.5"> {/* Cambiado a m-0.5 para espaciado de 2 píxeles */}
+          <div key={ticket.ticketId} className="m-0.5"> 
             <TicketCard 
               title={ticket.title}
               description={ticket.description}
@@ -104,6 +107,7 @@ const StatusColumn = ({ title, tickets, handleNextSection, handlePrevSection, sh
 );
 
 export default TicketsStatus;
+
 
 
 
