513baadbe59fde65aacec6eea33611b5ceb2e3d3
Joseph Colin <joelbenjosephc@gmail.com>
2024-10-02 20:06:33 -0600

    Se agregan validaciones a inicio de sesion

--- pages/inicioSesion.jsx
+++ pages/inicioSesion.jsx
@@ -3,7 +3,7 @@ import { Montserrat, Source_Sans_3 } from 'next/font/google'
 import Link from 'next/link'
 import { toast, Toaster } from 'sonner'
 import { useForm } from 'react-hook-form'
-import { useState } from "react"
+import { useState } from 'react'
 import { login } from '../pages/api/api'
 import { useRouter } from 'next/router'
 
@@ -37,10 +37,27 @@ const LoginForm = ({ textColor, bgColor }) => {
 
   const onSubmit = async (data) => {
     try {
-      const token = await login(data.email, data.password)
+      // Validación de campos vacíos
+      if (!data.email) {
+        setError('email', {
+          type: 'manual',
+          message: 'El correo electrónico es requerido'
+        })
+        return
+      }
 
-      if (token) {
-        window.localStorage.setItem('token', token)
+      if (!data.password) {
+        setError('password', {
+          type: 'manual',
+          message: 'La contraseña es requerida'
+        })
+        return
+      }
+
+      const response = await login(data.email, data.password)
+
+      if (response.token) {
+        window.localStorage.setItem('token', response.token)
         toast.success('Bienvenido', {
           position: window.innerWidth < 640 ? 'top-center' : 'bottom-left', // top-center para pantallas pequeñas
           style: {
@@ -51,26 +68,53 @@ const LoginForm = ({ textColor, bgColor }) => {
           }
         })
         setTimeout(() => {
-          router.push('/ticketsDashboard') // Redirige al resetPassword después de enviar el correo
+          router.push('/ticketsDashboard') // Redirige al dashboard después de enviar el correo
         }, 2000)
       } else {
-        toast.error('Usuario o contraseña incorrectos')
-        setError('root.credentials', {
-          type: 'manual',
-          message: 'Credenciales Invalidas'
-        }) // en dado caso de que sea general
-
-        // setError("username", {type: "manual", message: "Usuario invalido"}) en caso de que eñ backend indique que el error es de un input
+        // Manejar errores específicos del backend
+        if (response.error === 'Correo electrónico no encontrado') {
+          setError('email', {
+            type: 'manual',
+            message: 'Correo electrónico no encontrado'
+          })
+        } else if (response.error === 'Contraseña incorrecta') {
+          setError('password', {
+            type: 'manual',
+            message: 'Contraseña incorrecta'
+          })
+        } else {
+          setError('root.credentials', {
+            type: 'manual',
+            message: 'Credenciales Invalidas'
+          })
+        }
+        toast.error(response.error, {
+          position: window.innerWidth < 640 ? 'top-center' : 'bottom-left', // top-center para pantallas pequeñas
+          style: {
+            fontSize: '20px',
+            padding: '20px',
+            maxWidth: '90vw', // Ajuste para pantallas pequeñas
+            width: 'auto'
+          }
+        })
       }
     } catch (error) {
-      toast.error('Error al iniciar sesion')
+      toast.error('Usuario o contraseña incorrectos', {
+        position: window.innerWidth < 640 ? 'top-center' : 'bottom-left', // top-center para pantallas pequeñas
+        style: {
+          fontSize: '20px',
+          padding: '20px',
+          maxWidth: '90vw', // Ajuste para pantallas pequeñas
+          width: 'auto'
+        }
+      })
       console.error('[Login error]', error)
     }
   }
 
-    function handleShowHidePassword() {
-      setShowPassword(!showPassword)
-    }
+  function handleShowHidePassword() {
+    setShowPassword(!showPassword)
+  }
 
   return (
     <form
@@ -109,7 +153,7 @@ const LoginForm = ({ textColor, bgColor }) => {
               </div>
               {errors[item.name] && (
                 <span className='mt-1 text-red-500'>
-                  Este campo es requerido
+                  {errors[item.name].message || 'Este campo es requerido'}
                 </span>
               )}
             </div>
