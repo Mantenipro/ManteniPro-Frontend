fd858999ef78d39a3a4ecbb81b6b4931ed7396e9
Luis Casas <luisscasas45@gmail.com>
2024-10-28 17:14:43 -0600

    FEAT/  API integration commit.

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://mantenipro-api-4.onrender.com';
+const API_URL = 'http://localhost:8000';
 
 export async function createReport(data) {
     const res = await fetch(`${API_URL}/report`, {
@@ -11,3 +11,75 @@ export async function createReport(data) {
     const json = await res.json();
     return json.data;
   }
+
+
+// Función para crear un equipo
+export async function createEquipment(
+    equipmentName,
+    model,
+    company,
+    owner,
+    manufactureDate,
+    brand,
+    location,
+    unitType,
+    image,
+    qr,
+    token
+) {
+    try {
+        const res = await fetch(`${API_URL}/equipment`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token,
+            },
+            body: JSON.stringify({
+                equipmentName,
+                model,
+                company,
+                owner,
+                manufactureDate,
+                brand,
+                location,
+                unitType,
+                image,
+                qr,
+            }),
+        });
+
+        if (!res.ok) {
+            throw new Error(`Error: ${res.status} ${res.statusText}`);
+        }
+
+        const json = await res.json();
+        return json.data; 
+    } catch (error) {
+        console.error("Error creating equipment:", error);
+        throw error; 
+    }
+}
+
+// Función para obtener usuarios (sin necesidad de token)
+export async function getUsers(token) {
+    try {
+        const res = await fetch(`${API_URL}/register`, {
+            method: 'GET',
+            headers: {
+                Authorization: token,
+            },
+        });
+
+        if (!res.ok) {
+            throw new Error(`Error: ${res.status} ${res.statusText}`);
+        }
+
+        const json = await res.json();
+        return json.data.posts; // Asegúrate de que esto sea lo que realmente deseas devolver
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
+    }
+}
+
+      
--- components/FormEquipment.jsx
+++ components/FormEquipment.jsx
@@ -1,242 +1,184 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/router'; 
+import { useRouter } from 'next/router';
+import { createEquipment } from '@/api/api';
 import { Source_Sans_3 } from 'next/font/google';
-import { toast, Toaster } from 'sonner';
 
 const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
-const FormEquipment = ({ initialData }) => {
-  const { register, handleSubmit, setValue, getValues } = useForm({
-    defaultValues: initialData || {}, 
-  });
+export default function FormEquipment() {
+  const { handleSubmit, register, formState: { errors } } = useForm();
+  const router = useRouter();
 
-  const router = useRouter(); 
+  async function onSubmit(data) {
+    const token = localStorage.getItem("token");
 
-  useEffect(() => {
-    if (initialData) {
-      Object.keys(initialData).forEach((key) => {
-        setValue(key, initialData[key]);
-      });
-    }
-  }, [initialData, setValue]);
-
-  const handleFormSubmit = (data) => {
-    const missingFields = [];
-    
-    if (!data.nombreEquipo) missingFields.push('Nombre del equipo');
-    if (!data.modelo) missingFields.push('Modelo');
-    if (!data.numeroSerie) missingFields.push('Número de serie');
-    if (!data.propietario) missingFields.push('Propietario');
-    if (!data.marca) missingFields.push('Marca');
-    if (!data.ubicacion) missingFields.push('Ubicación');
-    if (!data.tipoUnidad) missingFields.push('Tipo de unidad');
-
-    if (missingFields.length > 0) {
-      toast.error(`Faltan campos por llenar: ${missingFields.join(', ')}`, {
-        position: 'bottom-right',
-        style: {
-          fontSize: '16px',
-          padding: '10px',
-          maxWidth: '90vw', 
-          width: 'auto'
-        }
-      });
-      return; 
-    }
-
-    console.log(data); 
-
-    if (initialData) {
-      router.push('/qrActualizado'); 
+    if (token) {
+      try {
+        // Llama a createEquipment con los datos ingresados en el formulario
+        await createEquipment(
+          data.equipmentName,
+          data.model,
+          null, 
+          data.owner,
+          data.manufactureDate,
+          data.brand,
+          data.location,
+          data.unitType,
+          data.image || null, 
+          null, 
+          token
+        );
+        router.push("/inventarioEquipos"); 
+      } catch (error) {
+        console.error("Error creating equipment:", error);
+      }
     } else {
-      router.push('/qrCreado'); 
+      console.error("Token not found");
     }
-  };
+  }
 
   return (
-    <>
-      <Toaster />
-      <form
-        onSubmit={handleSubmit(handleFormSubmit)}
-        className={`${sourceSans3.className} bg-white shadow-lg rounded-lg px-4 pt-1 w-full max-w-[30rem] min-h-[30rem] flex flex-col`}
-      >
-        <div className='space-y-4 flex-1'>
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' 
-                htmlFor='nombreEquipo'
-              >
-                Nombre del equipo
-              </label>
-              <input
-                {...register('nombreEquipo')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='nombreEquipo'
-                type='text'
-                placeholder='Nombre del equipo'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' 
-                htmlFor='modelo'
-              >
-                Modelo
-              </label>
-              <input
-                {...register('modelo')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='modelo'
-                type='text'
-                placeholder='Modelo del equipo'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='numeroSerie'
-              >
-                Número de serie
-              </label>
-              <input
-                {...register('numeroSerie')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='numeroSerie'
-                type='text'
-                placeholder='Número de serie'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='propietario'
-              >
-                Propietario
-              </label>
-              <input
-                {...register('propietario')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='propietario'
-                type='text'
-                placeholder='Propietario del equipo'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='fechaFabricacion'
-              >
-                Última fecha de mantenimiento
-              </label>
-              <input
-                {...register('fechaFabricacion')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='fechaFabricacion'
-                type='date'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='marca'
-              >
-                Marca
-              </label>
-              <input
-                {...register('marca')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='marca'
-                type='text'
-                placeholder='Marca del equipo'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='ubicacion'
-              >
-                Ubicación
-              </label>
-              <input
-                {...register('ubicacion')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='ubicacion'
-                type='text'
-                placeholder='Ubicación del equipo'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='tipoUnidad'
-              >
-                Tipo de unidad
-              </label>
-              <input
-                {...register('tipoUnidad')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='tipoUnidad'
-                type='text'
-                placeholder='Tipo de unidad'
-              />
-            </div>
-          </div>
-
-          <div className='mb-4'>
-            <div className='w-full'>
-              <label
-                className='block text-gray-700 text-sm font-semibold mb-[2px] text-left'
-                htmlFor='foto'
-              >
-                Agregar foto (opcional)
-              </label>
-              <input
-                {...register('foto')}
-                className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
-                id='foto'
-                type='file'
-              />
-            </div>
-          </div>
-
-          <div className='flex justify-center lg:mt-4 lg:mb-10 mb-14 mt-32'>
-            <button
-              type='submit'
-              className='py-2 px-4 bg-gradient-to-r from-[#21262D] to-[#414B66] text-white font-bold rounded-lg shadow-md hover:from-[#1a1d24] hover:to-[#373f5a] focus:outline-none focus:ring-4 focus:ring-blue-300'
-            >
-              {initialData ? 'Actualizar' : 'Agregar'}
-            </button>
-          </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={`${sourceSans3.className} bg-white shadow-lg rounded-lg px-4 pt-1 w-full max-w-[30rem] min-h-[30rem] flex flex-col`}
+    >
+      <div className='space-y-4 flex-1'>
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='equipmentName'>
+            Nombre del equipo
+          </label>
+          <input
+            {...register('equipmentName', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='equipmentName'
+            type='text'
+            placeholder='Nombre del equipo'
+          />
+          {errors.equipmentName && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
         </div>
-      </form>
-    </>
-  );
-};
 
-export default FormEquipment;
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='model'>
+            Modelo
+          </label>
+          <input
+            {...register('model', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='model'
+            type='text'
+            placeholder='Modelo del equipo'
+          />
+          {errors.model && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='owner'>
+            Propietario
+          </label>
+          <input
+            {...register('owner', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='owner'
+            type='text'
+            placeholder='Propietario del equipo'
+          />
+          {errors.owner && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='manufactureDate'>
+            Fecha de fabricación
+          </label>
+          <input
+            {...register('manufactureDate', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='manufactureDate'
+            type='date'
+          />
+          {errors.manufactureDate && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='brand'>
+            Marca
+          </label>
+          <input
+            {...register('brand', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='brand'
+            type='text'
+            placeholder='Marca del equipo'
+          />
+          {errors.brand && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='location'>
+            Ubicación
+          </label>
+          <input
+            {...register('location', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='location'
+            type='text'
+            placeholder='Ubicación del equipo'
+          />
+          {errors.location && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='unitType'>
+            Tipo de unidad
+          </label>
+          <input
+            {...register('unitType', { required: true })}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='unitType'
+            type='text'
+            placeholder='Tipo de unidad'
+          />
+          {errors.unitType && <span className="text-red-500 text-sm">Este campo es obligatorio</span>}
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-semibold mb-[2px] text-left' htmlFor='image'>
+            Agregar URL de foto (opcional)
+          </label>
+          <input
+            {...register('image')}
+            className='appearance-none border border-gray-300 rounded-lg w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500'
+            id='image'
+            type='text'
+            placeholder='Ingresa la URL de la imagen'
+          />
+        </div>
+
+        <div className='flex justify-center lg:mt-4 lg:mb-10 mb-14 mt-32'>
+          <button
+            type='submit'
+            className='py-2 px-4 bg-gradient-to-r from-[#21262D] to-[#414B66] text-white font-bold rounded-lg shadow-md hover:from-[#1a1d24] hover:to-[#373f5a] focus:outline-none focus:ring-4 focus:ring-blue-300'
+          >
+            Agregar
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
