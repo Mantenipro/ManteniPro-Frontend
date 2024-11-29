8705e068d22faa06ad2e47156ca8d5f24e042701
Luis Casas <luisscasas45@gmail.com>
2024-11-29 14:56:50 -0600

    FEAT/ AWS' url

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = 'https://api-v1.mantenipro.net';
 
 export async function login(email, password) {
   try {
@@ -406,7 +406,6 @@ export const fetchUserProfile = async () => {
     }
 
     const data = await response.json()
-    console.log('User profile data:', data)
     return data
   } catch (error) {
     console.error('Error fetching user profile:', error)
@@ -461,7 +460,7 @@ export const fetchUsers = async () => {
     if (data.success) {
       // Filtrar usuarios por rol
       const usuariosFiltrados = data.data.users.filter(
-        (user) => user.role === 'usuario' || user.role === 'admin'
+        (user) => user.role === 'usuario'
       )
       return usuariosFiltrados
     } else {
@@ -473,30 +472,6 @@ export const fetchUsers = async () => {
     return []
   }
 }
-
-export const getCurrentUser = async () => {
-  try {
-    const token = localStorage.getItem('token'); // Reemplaza con tu token real
-    const response = await fetch(`${API_URL}/users/profile`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await response.json();
-    if (data.success) {
-      console.log('Usuario actual:', data.data);
-      return data.data
-    } else {
-      console.error('Error al obtener el usuario actual:', data.error);
-      return null;
-    }
-  } catch (error) {
-    console.error('Error al hacer la solicitud:', error);
-    return null;
-  }
-};
 
 export const fetchTechnician = async () => {
   try {
