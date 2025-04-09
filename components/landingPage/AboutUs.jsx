a1ceca09d21d6376c14ae78a8ada6d24e057889b
Luis Casas <luisscasas45@gmail.com>
2025-04-09 17:14:09 -0600

    FEAT/ FEAT/ Update AboutUs and Nav components for improved layout and content; enhance form handling and styling

--- components/landingPage/AboutUs.jsx
+++ components/landingPage/AboutUs.jsx
@@ -3,29 +3,50 @@ const sourceSans3 = Source_Sans_3({ subsets: ['latin'] })
 
 export default function AboutUs() {
   return (
-    <div className='md:flex md:justify-center mx-5'>
-    <section className='mx-auto mb-20 flex w-full flex-col items-center justify-center gap-10 bg-[#e6eef8] p-4 md:px-10 lg:mt-36 lg:flex-row'>
-      <h2 className='mb-5 text-center text-2xl font-medium text-slate-800 lg:hidden'>
-        Acerca de Mantenipro
-      </h2>
-      <div className='min-w-56 lg:-mt-28'>
-        <img src='/img-aboutUs.png' alt='about us' className='h-full w-full' />
-      </div>
-      <div className='max-w-screen-md'>
-        <h2 className='mb-8 hidden text-center text-4xl font-medium text-slate-800 lg:block'>
+    <div className='mx-5 md:flex md:justify-center'>
+      <section className='mx-auto mb-20 flex w-full flex-col items-center justify-center gap-10 bg-[#e6eef8] p-4 md:px-10 lg:mt-36 lg:flex-row'>
+        <h2 className='mb-5 text-center text-2xl font-medium text-slate-800 lg:hidden'>
           Acerca de Mantenipro
         </h2>
-        <p
-          className={`text-center text-lg text-slate-700 md:mx-10 md:text-xl ${sourceSans3.className}`}
-        >
-          En Mantenipro, simplificamos el mantenimiento de equipos con
-          tecnología eficiente, reduciendo costos y optimizando procesos. Nos
-          enfocamos en soluciones fáciles de usar que eliminan la necesidad de
-          largas llamadas para obtener datos, permitiéndote concentrarte en el
-          crecimiento de tu negocio.
-        </p>
-      </div>
-    </section>
+        <div className='min-w-56 lg:-mt-28'>
+          <img
+            src='/img-aboutUs.png'
+            alt='about us'
+            className='h-full w-full'
+          />
+        </div>
+        <div className='max-w-screen-md'>
+          <h2 className='mb-8 hidden text-center text-4xl font-medium text-slate-800 lg:block'>
+            Acerca de Mantenipro
+          </h2>
+          <p
+            className={`text-center text-lg text-slate-700 md:mx-10 md:text-xl ${sourceSans3.className}`}
+          >
+            En Mantenipro, simplificamos el mantenimiento de equipos con
+            tecnología eficiente, reduciendo costos y optimizando procesos. Nos
+            enfocamos en soluciones fáciles de usar que eliminan la necesidad de
+            largas llamadas para obtener datos, permitiéndote concentrarte en el
+            crecimiento de tu negocio. Nuestro software está diseñado para
+            atender una amplia gama de sectores, cubriendo equipos de
+            climatización, energía, tecnología, construcción, elevación,
+            seguridad e industriales. Lo que nos diferencia es que no solo
+            ofrecemos soluciones fáciles, sino que también te proporcionamos un
+            historial detallado para tomar decisiones informadas. En un mercado
+            donde pocos softwares están tan dedicados como el nuestro,
+            Mantenipro se posiciona como la solución ideal para la gestión de
+            mantenimiento en sectores industriales y comerciales. <br />
+            Ingresa a la{' '}
+            <a
+              href='https://drive.google.com/file/d/1KiukB2Eacy1CVe79EIvpoLmGSpDFCuAe/view?usp=sharing'
+              target='_blank'
+              className='text-blue-500'
+            >
+              lista completa de los equipos
+            </a>{' '}
+            para conocer todos los equipos que puedes registrar.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
--- components/landingPage/Nav.jsx
+++ components/landingPage/Nav.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import emailjs from 'emailjs-com';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Nav() {
-  const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false)
   const [formData, setFormData] = useState({
     name: '',
     lastname: '',
@@ -12,43 +12,46 @@ export default function Nav() {
     phone: '',
     date: '',
     time: ''
-  });
+  })
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
 
   const isSunday = (dateString) => {
-    const date = new Date(dateString);
-    return date.getDay() === 0; // 0 corresponde a domingo
-  };
+    const date = new Date(dateString)
+    return date.getDay() === 0 // 0 corresponde a domingo
+  }
 
   const isValidTime = (timeString) => {
-    const [hours, minutes] = timeString.split(':').map(Number);
-    return hours >= 9 && (hours < 20 || (hours === 20 && minutes === 0)); // Entre 9:00 y 20:00
-  };
+    const [hours, minutes] = timeString.split(':').map(Number)
+    return hours >= 9 && (hours < 20 || (hours === 20 && minutes === 0)) // Entre 9:00 y 20:00
+  }
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (isSunday(formData.date)) {
-      alert('No se pueden agendar demos los domingos.');
-      return;
+      alert('No se pueden agendar demos los domingos.')
+      return
     }
 
     if (!isValidTime(formData.time)) {
-      alert('El horario debe estar entre las 9:00 AM y las 8:00 PM.');
-      return;
+      alert('El horario debe estar entre las 9:00 AM y las 8:00 PM.')
+      return
     }
 
-    const serviceId = 'service_b41h2e5';
-    const templateId = 'template_wzg9xkj';
-    const userId = 'z2U466-wfiojGUUIA';
+    const serviceId = 'service_b41h2e5'
+    const templateId = 'template_wzg9xkj'
+    const userId = 'z2U466-wfiojGUUIA'
 
-    emailjs.send(serviceId, templateId, formData, userId)
+    emailjs
+      .send(serviceId, templateId, formData, userId)
       .then((response) => {
-        alert('¡Correo enviado exitosamente, nos pondremos en contacto contigo lo mas pronto posible!');
+        alert(
+          '¡Correo enviado exitosamente, nos pondremos en contacto contigo lo mas pronto posible!'
+        )
         setFormData({
           name: '',
           lastname: '',
@@ -56,44 +59,44 @@ export default function Nav() {
           phone: '',
           date: '',
           time: ''
-        });
-        setShowForm(false);
+        })
+        setShowForm(false)
       })
       .catch((error) => {
-        alert('Hubo un error al enviar el correo.');
-        console.error(error);
-      });
-  };
+        alert('Hubo un error al enviar el correo.')
+        console.error(error)
+      })
+  }
 
   return (
-    <nav className='flex md:justify-between shadow-lg bg-white h-[80px] p-2'>
-      <div className='flex justify-center items-center md:w-[17rem] mr-4'>
+    <nav className='flex h-[80px] bg-white p-2 shadow-lg md:justify-between'>
+      <div className='mr-4 flex items-center justify-center md:w-[17rem]'>
         <img src='/logoManteniPro.svg' alt='Logo ManteniPro' />
       </div>
 
       <div className='flex items-center space-x-2 md:space-x-4'>
         <Link href='/inicioSesion'>
-          <button className='rounded-xl text-gray-900 border-[1px] border-gray-700 p-[10px_20px] md:text-base text-xs font-medium whitespace-nowrap hover:bg-gradient-to-r hover:from-[#21262D] hover:to-[#414B66] hover:text-white'>
+          <button className='whitespace-nowrap rounded-xl border-[1px] border-gray-700 p-[10px_20px] text-xs font-medium text-gray-900 hover:bg-gradient-to-r hover:from-[#21262D] hover:to-[#414B66] hover:text-white md:text-base'>
             Iniciar Sesión
           </button>
         </Link>
 
         <Link href='/registroUsuario'>
-          <button className='rounded-xl text-gray-900 border-[1px] border-gray-700 p-[10px_20px] md:text-base text-xs font-medium hover:bg-gradient-to-r hover:from-[#21262D] hover:to-[#414B66] hover:text-white'>
+          <button className='rounded-xl border-[1px] border-gray-700 p-[10px_20px] text-xs font-medium text-gray-900 hover:bg-gradient-to-r hover:from-[#21262D] hover:to-[#414B66] hover:text-white md:text-base'>
             Registrarme
           </button>
         </Link>
 
-        <button
+        {/* <button
           onClick={() => setShowForm(!showForm)}
           className='rounded-xl text-gray-900 border-[1px] border-gray-700 p-[10px_20px] md:text-base text-xs font-medium hover:bg-gradient-to-r hover:from-[#21262D] hover:to-[#414B66] hover:text-white'>
           Agendar Demo
-        </button>
+        </button> */}
       </div>
 
       {showForm && (
-        <div className='absolute top-[90px] right-[10px] bg-white shadow-lg p-6 rounded-lg w-[300px] md:w-[400px]'>
-          <h2 className='text-xl font-semibold mb-4'>Agendar Demo</h2>
+        <div className='absolute right-[10px] top-[90px] w-[300px] rounded-lg bg-white p-6 shadow-lg md:w-[400px]'>
+          <h2 className='mb-4 text-xl font-semibold'>Agendar Demo</h2>
           <form onSubmit={sendEmail}>
             <div className='mb-3'>
               <label className='block text-sm font-medium'>Nombre</label>
@@ -102,7 +105,7 @@ export default function Nav() {
                 name='name'
                 value={formData.name}
                 onChange={handleInputChange}
-                className='border rounded-lg p-2 w-full'
+                className='w-full rounded-lg border p-2'
                 required
               />
             </div>
@@ -113,7 +116,7 @@ export default function Nav() {
                 name='lastname'
                 value={formData.lastname}
                 onChange={handleInputChange}
-                className='border rounded-lg p-2 w-full'
+                className='w-full rounded-lg border p-2'
                 required
               />
             </div>
@@ -124,7 +127,7 @@ export default function Nav() {
                 name='email'
                 value={formData.email}
                 onChange={handleInputChange}
-                className='border rounded-lg p-2 w-full'
+                className='w-full rounded-lg border p-2'
                 required
               />
             </div>
@@ -135,7 +138,7 @@ export default function Nav() {
                 name='phone'
                 value={formData.phone}
                 onChange={handleInputChange}
-                className='border rounded-lg p-2 w-full'
+                className='w-full rounded-lg border p-2'
                 required
               />
             </div>
@@ -146,7 +149,7 @@ export default function Nav() {
                 name='date'
                 value={formData.date}
                 onChange={handleInputChange}
-                className='border rounded-lg p-2 w-full'
+                className='w-full rounded-lg border p-2'
                 required
               />
             </div>
@@ -157,20 +160,19 @@ export default function Nav() {
                 name='time'
                 value={formData.time}
                 onChange={handleInputChange}
-                className='border rounded-lg p-2 w-full'
+                className='w-full rounded-lg border p-2'
                 required
               />
             </div>
             <button
               type='submit'
-              className='w-full bg-gradient-to-r from-[#21262D] to-[#414B66] text-white p-2 rounded-lg'>
+              className='w-full rounded-lg bg-gradient-to-r from-[#21262D] to-[#414B66] p-2 text-white'
+            >
               Enviar
             </button>
           </form>
         </div>
       )}
     </nav>
-  );
+  )
 }
-
-
